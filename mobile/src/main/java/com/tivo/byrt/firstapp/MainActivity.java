cffb07710c5b41fb643d2fcbfe2ada57aebf62ec
byrtman <byrtman@gmail.com>
2019-04-29 17:10:42 -0700

    Augmented code based on CodeLabs learning units on certain components.

--- mobile/src/main/java/com/tivo/byrt/firstapp/MainActivity.java
+++ mobile/src/main/java/com/tivo/byrt/firstapp/MainActivity.java
@@ -5,10 +5,14 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.lang.reflect.Array;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String EXTRA_MESSAGE = "com.tivo.byrt.firstApp.MESSAGE";
+    private static final String[] tickers = {"BAC","HASI","IBM","AAPL","TIVO","FB","GOOGL"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,8 +23,25 @@ public class MainActivity extends AppCompatActivity {
     public void sendMessage(View view) {
         // Finish
         Intent intent = new Intent(this, DisplayMessageActivity.class);
-        String message = "BAC";
+        int i = (int) Math.round(Math.random() * tickers.length);
+        String message = tickers[i];
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    public void onImageClick(View view) {
+        displayToast("Ouch!");
+    }
+
+    public void onClickFAB(View view) {
+        Intent intent = new Intent(MainActivity.this, OrderActivity.class);
+        startActivity(intent);
+    }
+
+
 }
---
+++ mobile/src/main/java/com/tivo/byrt/firstapp/OrderActivity.java
@@ -0,0 +1,72 @@
+package com.tivo.byrt.firstapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.RadioButton;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class OrderActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order);
+
+        Spinner spinner = findViewById(R.id.label_spinner);
+        if (spinner != null) {
+            spinner.setOnItemSelectedListener(this);
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                    R.array.labels_array, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            spinner.setAdapter(adapter);
+        }
+
+
+    }
+
+    public void onRadioButtonClicked(View view) {
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+        // Check which radio button was clicked.
+        switch (view.getId()) {
+            case R.id.sameDay:
+                if (checked)
+                    // Same day service
+                    displayToast(getString(R.string.same_day_messanger_service));
+                break;
+            case R.id.nextDay:
+                if (checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
+    }
+
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String spinnerLabel = parent.getItemAtPosition(position).toString();
+        displayToast(spinnerLabel);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
